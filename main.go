92a1d1f753cb5cadb42f6a720144c85edbdcba12
James Lucas <jamesmlucas@gmail.com>
2017-03-01 00:32:17 -0600

    updates

--- main.go
+++ main.go
@@ -1,10 +1,8 @@
 package main
 
-import "fmt"
 import "github.com/jaymell/gosmartcam/frameReader"
 
 func main() {
-	videoSource := "/dev/video3"
-	fr, _ := frameReader.NewFrameReader(videoSource)
-	fmt.Println(fr)
+	videoSource := "/dev/video1"
+	_, _ = frameReader.NewFrameReader(videoSource)
 }
